34330bbd7724a19425e767afd3dd7277fe33fcca
bekkos <bekkosm@gmail.com>
2026-02-26 15:38:31 +0100

    Added WeatherRandomizerService

---
+++ GW-server-plugin/Patches/MissionSaveLoadPatches.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using GW_server_plugin.Features;
+using HarmonyLib;
+using NuclearOption.SavedMission;
+
+namespace GW_server_plugin.Patches;
+
+
+
+[HarmonyPatch(typeof(MissionSaveLoad))]
+[HarmonyPriority(Priority.First)]
+[HarmonyWrapSafe]
+public class MissionSaveLoadPatches
+{
+    [HarmonyPrefix]
+    [HarmonyPatch(nameof(MissionSaveLoad.TryReadJson))]
+    private static void TryReadJsonPrefix(ref string json)
+    {
+        json = WeatherRandomizerService.RandomizeWeather(json);
+    }
+}
