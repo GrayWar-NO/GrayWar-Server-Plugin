3dbab30a1df5759d38dd2923b70bcdcae088c4d2
pauel <pau.lacazedieu@gmail.com>
2026-05-24 19:29:33 +0200

    Fixed weather randomizer.

--- GW-server-plugin/Patches/MissionSaveLoadPatches.cs
+++ GW-server-plugin/Patches/MissionSaveLoadPatches.cs
@@ -29,11 +29,12 @@ public class MissionSaveLoadPatches
         if (!PluginConfig.EnableWeatherRandomizer!.Value) return;
         var rnd = new Random();
         mission.environment.timeOfDay = rnd.Next(5, 18);
-        mission.environment.timeFactor = 8f;
-        mission.environment.weatherIntensity = (float)(rnd.NextDouble() * 0.8);
-        mission.environment.cloudAltitude = (float)(500 + rnd.NextDouble() * 1000);
-        mission.environment.windSpeed = (float)(rnd.NextDouble() * 4);
+        mission.environment.timeFactor = 1f; // TIME FACTOR goes from -2 to 3, values are [0, 0.5, 1, 10, 30, 60]
+        mission.environment.weatherIntensity = (float)rnd.NextDouble();
+        mission.environment.cloudAltitude = (float)(200 + rnd.NextDouble() * 1000);
+        mission.environment.windSpeed = (float)(rnd.NextDouble() * 10); // in m/s (10m/s is 36kph)
         mission.environment.windTurbulence = (float)rnd.NextDouble();
-        mission.environment.windHeading = rnd.Next(0, 360);
+        mission.environment.windHeading = rnd.Next(0, 360); 
+        mission.environment.windRandomHeading = rnd.Next(0, 180);
     }
 }
