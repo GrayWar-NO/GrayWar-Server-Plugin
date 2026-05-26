2d5006cfedf1409d25e5a748dd878d3178611f75
pauel <pau.lacazedieu@gmail.com>
2026-05-26 11:01:37 +0200

    Add moon phase to the fixes.

--- GW-server-plugin/Patches/LevelInfoLoadPatch.cs
+++ GW-server-plugin/Patches/LevelInfoLoadPatch.cs
@@ -12,6 +12,17 @@ public class LevelInfoLoadPatch
     public static void UpdatePostfix(LevelInfo __instance)
     {
         if (GameManager.ShowEffects) return;
+
+        if (__instance.moonPhase >= 0.0)
+        {
+            __instance.NetworkmoonPhase = __instance.moonPhase + (float) (__instance.timeFactor * (double) Time.deltaTime * 1.1574074051168282E-05);
+            if (__instance.moonPhase > 28.0)
+                __instance.NetworkmoonPhase = __instance.moonPhase - 28f;
+            __instance.SetMoonPhase(__instance.moonPhase);
+        }
+        
+        if (!Globals.NetworkManagerNuclearOptionInstance.Server.Active) return;
+
         __instance.NetworktimeOfDay = __instance.timeOfDay + (float) (__instance.timeFactor * (double) Time.deltaTime * 0.00027777778450399637);
         if (__instance.timeOfDay > 24.0)
             __instance.NetworktimeOfDay = __instance.timeOfDay - 24f;
