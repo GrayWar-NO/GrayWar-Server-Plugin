6a10c1757d1cc01ddbd5a652d7c1d977b5149925
bekkos <bekkosm@gmail.com>
2026-06-12 14:56:12 +0200

    Staff immune to votekicks within the built-in votekick system.

--- GW-server-plugin/Patches/VoteKickLogger.cs
+++ GW-server-plugin/Patches/VoteKickLogger.cs
@@ -1,3 +1,4 @@
+using System.Linq;
 using GW_server_plugin.Enums;
 using GW_server_plugin.Features.IPC.Packets;
 using HarmonyLib;
@@ -7,21 +8,32 @@ using Steamworks;
 namespace GW_server_plugin.Patches;
 
 /// <summary>
-///     Adds internal logging to votekicks
+///     Adds internal logging to votekicks and skips the kick entirely if it is a member of staff.
 /// </summary>
 [HarmonyPatch(typeof(VoteKickManager))]
 public class VoteKickLogger
 {
-    [HarmonyPostfix]
+
+    [HarmonyPrefix]
     [HarmonyPatch(nameof(VoteKickManager.ExecuteKick))]
     // ReSharper disable once InconsistentNaming
-    internal static void ExecuteKickPostfix(VoteKickManager __instance,ref CSteamID id, string playerName)
+    internal static bool ExecuteKickPrefix(VoteKickManager __instance, ref CSteamID id, string playerName)
     {
+        var steamId = id.m_SteamID.ToString();
+
+        var staffIds = PluginConfig.AdminsList
+            .Concat(PluginConfig.ModeratorsList)
+            .Concat(PluginConfig.Owner?.Value is { } owner ? [owner] : []);
+
+        if (staffIds.Contains(steamId))
+            return false;
+        
         GwServerPlugin.LoggingOutBox.Add(
             new LogEntryPacket
             {
                 Channel = LogChannel.Kick,
-                LogText = $"1:{id.m_SteamID.ToString()}:Votekicked",
+                LogText = $"1:{steamId}:Votekicked",
             });
+        return true;
     }
 }
