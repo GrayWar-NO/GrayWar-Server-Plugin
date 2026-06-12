490dc735cb35a5479b6c034719603bdf6dbca3a4
pauel <pau.lacazedieu@gmail.com>
2026-06-12 02:48:40 +0200

    fixed clearkicklist.

--- GW-server-plugin/Features/CommandUtils/Commands/ClearKickListCommand.cs
+++ GW-server-plugin/Features/CommandUtils/Commands/ClearKickListCommand.cs
@@ -1,3 +1,4 @@
+using System;
 using BepInEx.Configuration;
 using GW_server_plugin.Enums;
 using GW_server_plugin.Helpers;
@@ -11,13 +12,13 @@ namespace GW_server_plugin.Features.CommandUtils.Commands;
 public class ClearKickListCommand(ConfigFile config) : PermissionConfigurableCommand(config), IConsoleCommand, IGameCommand
 {
     /// <inheritdoc />
-    public override string Name { get; } = "clearkicklist";
+    public override string Name => "clearkicklist";
 
     /// <inheritdoc />
-    public override string Description { get; } = "Clears the integrated kick list";
+    public override string Description => "Clears the integrated kick list. Manual or vote clears only the manual kicks or the votekicks. Defaults to both.";
 
     /// <inheritdoc />
-    public override string Usage { get; } = "clearkicklist (takes no arguments)";
+    public override string Usage => "clearkicklist <optional 'manual' or 'vote'>";
 
     /// <inheritdoc />
     public bool Validate(Player player, string[] args) => Validate(args);
@@ -25,7 +26,10 @@ public class ClearKickListCommand(ConfigFile config) : PermissionConfigurableCom
     /// <inheritdoc />
     public bool Validate(string[] args)
     {
-        return args.Length == 0;
+        return args.Length == 0 ||
+               (args.Length == 1 &&
+                (StringComparer.OrdinalIgnoreCase.Equals(args[0], "manual") ||
+                 StringComparer.OrdinalIgnoreCase.Equals(args[0], "vote")));
     }
 
     /// <inheritdoc />
@@ -34,11 +38,17 @@ public class ClearKickListCommand(ConfigFile config) : PermissionConfigurableCom
     /// <inheritdoc />
     public bool Execute(string[] args, out string? response)
     {
-        Globals.NetworkManagerNuclearOptionInstance.Authenticator.KickList.Clear();
-        response = "Kick list cleared successfully!";
+        var mode = args.Length > 0 ? args[0] : null;
+
+        if (!StringComparer.OrdinalIgnoreCase.Equals(mode, "vote"))
+            Globals.NetworkManagerNuclearOptionInstance.Authenticator.KickList.Clear();
+
+        if (!StringComparer.OrdinalIgnoreCase.Equals(mode, "manual"))
+            Globals.NetworkManagerNuclearOptionInstance.Authenticator.MissionKickList.Clear();
+
+        response = $"{mode}Kick list cleared successfully!";
         return true;
     }
-
     /// <inheritdoc />
-    public override PermissionLevel DefaultPermissionLevel { get; } = PermissionLevel.Admin;
+    public override PermissionLevel DefaultPermissionLevel => PermissionLevel.Admin;
 }
