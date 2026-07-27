96ea4259a057bb89ccfe81f04e030a0964e360ed
jmarmstrong1207 <jm.armstrong1207@gmail.com>
2026-07-27 02:52:42 -0700

    Add voteskip command

---
+++ GW-server-plugin/Features/CommandUtils/Commands/VoteSkipCommand.cs
@@ -0,0 +1,71 @@
+using System;
+using BepInEx.Configuration;
+using Com.Graywar.NoServerManager.Proto;
+using Cysharp.Threading.Tasks;
+using Google.Protobuf.WellKnownTypes;
+using NuclearOption.Networking;
+using NuclearOption.SavedMission.ObjectiveV2.Objectives;
+
+namespace GW_server_plugin.Features.CommandUtils.Commands;
+
+/// <summary>
+/// Vote to skip current mission
+/// </summary>
+/// <param name="config"></param>
+[AutoCommand]
+public class VoteSkipCommand(ConfigFile config) : PermissionConfigurableCommand(config), IGameCommand
+{
+    /// <inheritdoc />
+    public override string Name => "voteskip";
+
+    /// <inheritdoc />
+    public override string Description => "Let you skip this mission by voting";
+
+    /// <inheritdoc />
+    public override string Usage => "voteskip to initiate a vote to skip the current mission";
+
+    /// <inheritdoc />
+    public override PermissionLevel DefaultPermissionLevel => PermissionLevel.Everyone;
+
+    /// <inheritdoc />
+    public UniTask<bool> Validate(Player player, string[] args)
+    {
+        return UniTask.FromResult(true);
+    }
+
+    /// <inheritdoc />
+    public UniTask<(bool success, string? response)> Execute(Player player, string[] args)
+    {
+        if (!GenericVoteService.CanStartVote())
+        {
+            var response = "Cannot start a new vote, please wait for current vote to expire.";
+            return UniTask.FromResult<(bool success, string? response)>((false, response));
+        }
+
+        void OnPass()
+        {
+            var log = new ChatLog
+            {
+                MessageChannel = "all",
+                MessageSendTime = DateTime.UtcNow.ToTimestamp(),
+                Message = "Voteskip has passed",
+                SenderSteamID = player.SteamID
+            };
+            GwServerPlugin.GrpcMgr.ChatLogStream?.WriteAsync(log);
+
+            _ = MissionService.StartNextMission();
+        }
+
+        var startingMessage = "A vote to skip the current mission has been started";
+        ChatService.SendChatMessageAsServer(startingMessage);
+
+        GenericVoteService.StartVote(
+            player,
+            OnPass,
+            true,
+            false,
+            "Voteskip current mission"
+        );
+        return UniTask.FromResult<(bool success, string? response)>((true, null));
+    }
+}
