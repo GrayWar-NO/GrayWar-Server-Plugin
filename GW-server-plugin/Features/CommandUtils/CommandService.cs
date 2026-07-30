7bd8807545d13ed3dd515408e7a3e229b7a88a00
jmarmstrong1207 <jm.armstrong1207@gmail.com>
2026-07-29 17:09:22 -0700

    Add Description to response of invalid command arguments

--- GW-server-plugin/Features/CommandUtils/CommandService.cs
+++ GW-server-plugin/Features/CommandUtils/CommandService.cs
@@ -176,7 +176,7 @@ public static class CommandService
 
         GwServerPlugin.Logger.LogWarning(
             $"Failed validation for command {command.Name} by remote process with argument(s): {string.Join(", ", args)}");
-        response = $"Invalid arguments for command {command.Name}\n{PluginConfig.CommandPrefix!.Value}{command.Usage}";
+        response = $"Invalid arguments for command {command.Name}\nDescription: {command.Description}\n{PluginConfig.CommandPrefix!.Value}{command.Usage}";
         return (false, response);
     }
 }
